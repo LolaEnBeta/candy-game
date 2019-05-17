11edc9503982e612ce0810e1c82ebb9519149ff8
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-17 16:45:05 +0200

    Delete runes

--- candy-game.py
+++ candy-game.py
@@ -21,8 +21,29 @@ def generate_rune(board):
         for column_index in range(len(board)):
             board[row_index][column_index] = random.choice(["X", "O", "H"])
 
+def delete_runes(board, row, column, choosen_rune):
+    for row_index in range(row - 1, row + 2):
+        if row_index == row:
+            for column_index in range(column - 1, column +2):
+                if board[row_index][column_index] == board[row][column]:
+                    board[row_index][column_index] = "-"
+                else:
+                    if board[row_index][column_index] == choosen_rune:
+                        board[row_index][column_index] = "-"
+        else:
+            if board[row_index][column] == choosen_rune:
+                board[row_index][column] = "-"
+
 candy_game = create_board(board_size)
 
 generate_rune(candy_game)
+
+print_board(candy_game)
+
+row = int(input("Choose one row: "))
+column = int(input("Choose one column: "))
+choosen_rune = candy_game[row][column]
+
+delete_runes(candy_game, row, column, choosen_rune)
 
 print_board(candy_game)

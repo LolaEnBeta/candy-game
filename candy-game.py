b196b62260338b8ced8cfdb49fc5c6f9c3d6d42b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-17 16:07:05 +0200

    Generate runes

--- candy-game.py
+++ candy-game.py
@@ -1,4 +1,4 @@
-
+import random
 board_size = 5
 
 def create_board(board_size):
@@ -16,6 +16,13 @@ def print_board(board):
             print(column, "", end="")
         print("")
 
+def generate_rune(board):
+    for row_index in range(len(board)):
+        for column_index in range(len(board)):
+            board[row_index][column_index] = random.choice(["X", "O", "H"])
+
 candy_game = create_board(board_size)
+
+generate_rune(candy_game)
 
 print_board(candy_game)
